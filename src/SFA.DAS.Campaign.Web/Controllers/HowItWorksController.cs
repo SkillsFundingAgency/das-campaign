7246ac2236dfc8a3e0a665e179b4bfd5ce74affb
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-10-22 10:14:45 +0100

    Add how it works controller

---
+++ src/SFA.DAS.Campaign.Web/Controllers/HowItWorksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Campaign.Web.Controllers
+{
+    public class HowItWorksController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
