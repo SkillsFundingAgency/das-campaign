804b111e48b5ca8a62b8eadcedadfcd899a9d687
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-11-19 11:06:40 +0000

    Stub implementation for data collection service
    Add application insights key into instrumentation initalisation

--- src/SFA.DAS.Campaign.Web/Startup.cs
+++ src/SFA.DAS.Campaign.Web/Startup.cs
@@ -61,7 +61,7 @@ namespace SFA.DAS.Campaign.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
