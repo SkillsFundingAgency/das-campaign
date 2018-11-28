8d46317cd89ce1cd1f79f336b0532fc5661dd0d2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-11-28 10:57:26 +0000

    Add controllers for areas

---
+++ src/SFA.DAS.Campaign.Web/Controllers/IndustriesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Campaign.Web.Controllers
+{
+    public class IndustriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
