1100910b584608495305f64103d8fd7560b68e64
Matt Derry <matt@goodoldmatt.com>
2018-11-28 11:45:06 +0000

    Adds button view component to web project

---
+++ src/SFA.DAS.Campaign.Web/Models/Components/Button/ButtonType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Campaign.Web.Models
+{
+    public enum ButtonType
+    {
+        Button=0,
+        Input=1,
+        Anchor=2
+
+    }
+}
---
+++ src/SFA.DAS.Campaign.Web/Views/Shared/Components/Button/button.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Campaign.Web.Models;
+
+namespace SFA.DAS.Campaign.Web.Views.Shared.Components.Button
+{
+    public class ButtonViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(ButtonType type,ButtonStyle style, string text, string classes = null, bool shadow = true, bool sparks = false, string href="#")
+        {
+            var button =new ButtonViewModel(style)
+            {
+                Text = text,
+                Class = classes,
+                Shadow = shadow,
+                Sparks = sparks,
+                Href = href
+            };
+
+            switch (type)
+            {
+                case ButtonType.Button:
+                    return View("Button",button);
+                    break;
+                case ButtonType.Input:
+                    return View("Input", button);
+                    break;
+                case ButtonType.Anchor:
+                    return View("Anchor", button);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+           
+            
+        }
+    }
+}
