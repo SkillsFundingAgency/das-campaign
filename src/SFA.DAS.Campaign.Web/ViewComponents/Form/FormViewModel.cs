f499e128158b993bcb91387f0342fa2e19278705
Matt Derry <matt@goodoldmatt.com>
2018-11-29 10:19:32 +0000

    Adds a basic form view component

---
+++ src/SFA.DAS.Campaign.Web/ViewComponents/Form/FormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Campaign.Web.ViewComponents.Form
+{
+    public class FormViewModel
+    {
+        public HttpMethod ResponseType { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+
+    }
+}
