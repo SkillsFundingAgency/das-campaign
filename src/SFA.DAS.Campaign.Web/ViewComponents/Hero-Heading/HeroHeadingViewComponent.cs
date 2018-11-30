19a069153d4f96142ecb5e5f5439b7eb1cd551af
Matt Derry <matt@goodoldmatt.com>
2018-11-30 11:23:04 +0000

    Adds basic hero heading view component and view

---
+++ src/SFA.DAS.Campaign.Web/ViewComponents/Hero-Heading/HeroHeadingViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Campaign.Web.ViewComponents.Modal;
+
+namespace SFA.DAS.Campaign.Web.ViewComponents
+{
+    public class HeroHeadingViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return View("Default");
+        }
+    }
+}
