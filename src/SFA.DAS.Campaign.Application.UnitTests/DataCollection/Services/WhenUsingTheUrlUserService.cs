106c7537c7a798ccf0fa74f9b7acf4d5a19a354f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-12-05 14:56:38 +0000

    Add user encoding service
    Added service that will encode and decode user ids. This will be used
    for creating unique pages for when a user wants to unsubscribe

---
+++ src/SFA.DAS.Campaign.Application.UnitTests/DataCollection/Services/WhenUsingTheUrlUserService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Campaign.Application.DataCollection.Services;
+using SFA.DAS.Campaign.Models.Configuration;
+
+namespace SFA.DAS.Campaign.Application.UnitTests.DataCollection.Services
+{
+    public class WhenUsingTheUrlUserService
+    {
+        private UserDataCryptographyService _userDataCryptographyService;
+        private Mock<IOptions<CampaignConfiguration>> _options;
+
+
+        [SetUp]
+        public void Arrange()
+        {
+            _options = new Mock<IOptions<CampaignConfiguration>>();
+            _options.Setup(x => x.Value.UserUrlSalt).Returns("Test Value");
+            _options.Setup(x => x.Value.UserUrlMinValue).Returns(10);
+            _options.Setup(x => x.Value.AllowedUrlCharacters).Returns("ABCDEFHJKLMNPRSTUV34689");
+
+            _userDataCryptographyService = new UserDataCryptographyService(_options.Object);
+        }
+
+        [Test]
+        public void Then_The_Configuration_Options_Are_Used_For_Generating_The_Encoded_Url()
+        {
+            //Act
+            _userDataCryptographyService.GenerateEncodedUserId(12345);
+
+            //Assert
+            _options.Verify(x=>x.Value.UserUrlMinValue, Times.Once);
+            _options.Verify(x=>x.Value.UserUrlSalt, Times.Once);
+            _options.Verify(x=>x.Value.AllowedUrlCharacters, Times.Once);
+        }
+
+        [Test]
+        public void Then_The_Encoded_String_Contains_The_UserId()
+        {
+            //Arrange
+            var userId = 12345;
+
+            //Act
+            var actual = _userDataCryptographyService.GenerateEncodedUserId(userId);
+
+            //Assert
+            Assert.AreNotEqual(string.Empty, actual);
+            Assert.AreNotEqual(userId, actual);
+        }
+
+        [Test]
+        public void Then_The_Encoded_String_Can_Be_Decoded_To_Get_The_User_Id()
+        {
+            //Arrange
+            var userId = 12345;
+
+            //Act
+            var encoded = _userDataCryptographyService.GenerateEncodedUserId(userId);
+            var actual = _userDataCryptographyService.DecodeUserId(encoded);
+
+            //Assert
+            Assert.AreEqual(userId, actual);
+        }
+
+        [Test]
+        public void Then_If_An_Invalid_Encoded_Value_Is_Passed_Then_Zero_Is_Returned()
+        {
+            //Act
+            var actual = _userDataCryptographyService.DecodeUserId("ZZ2211ASD");
+
+            //Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void Then_The_Encoded_String_Can_Be_Decoded_To_Get_The_User_Ids()
+        {
+            //Arrange
+            var userId = 12345;
+
+            //Act
+            var encoded = _userDataCryptographyService.GenerateEncodedUserId(userId);
+            var actual = _userDataCryptographyService.DecodeUserId(encoded);
+
+            //Assert
+            Assert.AreEqual(userId, actual);
+        }
+    }
+}
---
+++ src/SFA.DAS.Campaign.Application/DataCollection/Services/UserDataCryptographyService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HashidsNet;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Campaign.Models.Configuration;
+
+namespace SFA.DAS.Campaign.Application.DataCollection.Services
+{
+    public class UserDataCryptographyService
+    {
+        private readonly Hashids _hashedId;
+
+        public UserDataCryptographyService(IOptions<CampaignConfiguration> options)
+        {
+            _hashedId = new Hashids(options.Value.UserUrlSalt,options.Value.UserUrlMinValue,options.Value.AllowedUrlCharacters);
+        }
+        public string GenerateEncodedUserId(int userId)
+        {
+            return _hashedId.Encode(userId);
+        }
+
+        public long DecodeUserId(string encodedUrl)
+        {
+            try
+            {
+                return _hashedId.Decode(encodedUrl)[0];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+
+                return 0;
+            }
+            
+        }
+    }
+}
