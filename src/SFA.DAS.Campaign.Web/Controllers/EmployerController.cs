fee20a8fbc99dee7c506849fd96c0da652669ede
Matt Derry <matt@goodoldmatt.com>
2018-12-06 10:21:53 +0000

    adds some of the employer content

--- src/SFA.DAS.Campaign.Web/Controllers/EmployerController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/EmployerController.cs
@@ -6,9 +6,50 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
+    [Route("Employer")]
     public class EmployerController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("How-Much-Is-It-Going-To-Cost")]
+        public IActionResult HowMuchIsItGoingToCost()
+        {
+            return View();
+        }
+
+        public IActionResult TheRightApprenticeship()
+        {
+            return View();
+        }
+
+        public IActionResult ChooseATrainingProvider()
+        {
+            return View();
+        }
+
+        public IActionResult HireAnApprentice()
+        {
+            return View();
+        }
+
+        public IActionResult PreperationAndMonitoring()
+        {
+            return View();
+        }
+
+        public IActionResult AssessmentAndQualification()
+        {
+            return View();
+        }
+
+        public IActionResult WhatIsAnApprenticeship()
+        {
+            return View();
+        }
+
+        public IActionResult Benefits()
         {
             return View();
         }
