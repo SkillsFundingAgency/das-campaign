e1e02cc7c1ca718979925e91ca32c6f347eef603
James King <hello@jamesmichaelking.com>
2018-12-06 11:00:49 +0000

    Routes and navigation for employer

--- src/SFA.DAS.Campaign.Web/Controllers/EmployerController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/EmployerController.cs
@@ -6,49 +6,49 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
-    [Route("Employer")]
+    [Route("employer")]
     public class EmployerController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-        [Route("How-Much-Is-It-Going-To-Cost")]
+        [Route("how-much-is-it-going-to-cost")]
         public IActionResult HowMuchIsItGoingToCost()
         {
             return View();
         }
-
+        [Route("the-right-apprenticeship")]
         public IActionResult TheRightApprenticeship()
         {
             return View();
         }
-
+        [Route("choose-training-provider")]
         public IActionResult ChooseATrainingProvider()
         {
             return View();
         }
-
+        [Route("hire-an-apprentice")]
         public IActionResult HireAnApprentice()
         {
             return View();
         }
-
-        public IActionResult PreperationAndMonitoring()
+        [Route("preparing-and-monitoring")]
+        public IActionResult PreparingAndMonitoring()
         {
             return View();
         }
-
+        [Route("assessment-and-qualification")]
         public IActionResult AssessmentAndQualification()
         {
             return View();
         }
-
+        [Route("what-is-an-apprenticeship")]
         public IActionResult WhatIsAnApprenticeship()
         {
             return View();
         }
-
+        [Route("benefits")]
         public IActionResult Benefits()
         {
             return View();
