42b837ebc22c85214b99a929c88ba0a5c2d03b75
Matt Derry <matt@goodoldmatt.com>
2018-12-11 10:47:56 +0000

    Adds Apprentice Content to site

--- src/SFA.DAS.Campaign.Web/ViewComponents/Sidebar/SidebarViewComponent.cs
+++ src/SFA.DAS.Campaign.Web/ViewComponents/Sidebar/SidebarViewComponent.cs
@@ -10,40 +10,70 @@ namespace SFA.DAS.Campaign.Web.ViewComponents.Sidebar
         public async Task<IViewComponentResult> InvokeAsync(SidebarType type, int activeIndex, string imgLocation)
         {
             string view;
+            string title;
 
             switch (type)
             {
                 case SidebarType.Apprentice:
                     view = "../Shared/Sidebar/_Apprentice";
-                    if (imgLocation == null)
-                    {
-                        imgLocation = "/campaign/images/worker-girl.jpg";
-                    }
                     break;
                 case SidebarType.Employer:
                     view = "../Shared/Sidebar/_Employer";
-                    if (imgLocation == null)
-                    {
-                        imgLocation = "/campaign/images/worker-girl.jpg";
-                    }
+                    
                     break;
                 case SidebarType.None:
                     return Content(string.Empty);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), type, null);
             }
-            return View("Default", new SidebarViewModel(type, view, activeIndex, imgLocation));
+            return View("Default", new SidebarViewModel(type, view,null, activeIndex, imgLocation));
         }
     }
 
     public class SidebarViewModel
     {
-        public SidebarViewModel(SidebarType type, string contentView, int activeIndex, string imgLocation)
+        public SidebarViewModel(SidebarType type, string contentView,string title, int activeIndex, string imgLocation)
         {
             Type = type;
             ActiveIndex = activeIndex;
             ImgLocation = imgLocation;
             ContentView = contentView;
+            Title = title;
+
+            switch (type)
+            {
+                case SidebarType.Apprentice:
+                   
+
+                    if (imgLocation == null)
+                    {
+                        ImgLocation = "/campaign/images/worker-girl.jpg";
+                    }
+
+                    if (title == null)
+                    {
+
+                        Title = "the process to becoming an apprentice";
+                    }
+
+                    Classes = "sidebar-apprentice";
+                    break;
+                case SidebarType.Employer:
+                   
+                    if (imgLocation == null)
+                    {
+                        ImgLocation = "/campaign/images/worker-girl.jpg";
+                    }
+                    if (title == null)
+                    {
+
+                        Title = "the process to hire an apprentice";
+                    }
+                    Classes = "sidebar-apprentice";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
 
         }
 
@@ -54,6 +84,7 @@ namespace SFA.DAS.Campaign.Web.ViewComponents.Sidebar
         public int ActiveIndex { get; set; }
         public string ImgLocation { get; set; }
         public string Title { get; set; }
+        public string Classes { get; set; }
     }
 
 }
