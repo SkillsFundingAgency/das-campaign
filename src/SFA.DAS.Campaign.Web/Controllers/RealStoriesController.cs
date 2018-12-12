c747c929aaae88152f2b4ff6af6c2248e32af3d3
Matt Derry <matt@goodoldmatt.com>
2018-12-12 11:08:07 +0000

    adds real stories pages

--- src/SFA.DAS.Campaign.Web/Controllers/RealStoriesController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/RealStoriesController.cs
@@ -6,16 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
+    [Route("real-stories")]
     public class RealStoriesController : Controller
     {
+        [Route("apprentice")]
         public IActionResult Apprentice()
         {
             return View();
         }
+        [Route("employer")]
         public IActionResult Employer()
         {
             return View();
         }
+        [Route("famous-faces")]
         public IActionResult FamousFaces()
         {
             return View();
