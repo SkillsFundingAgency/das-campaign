b9e46790ace02554a5c22c66945fa40e0505be94
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-12-16 21:11:06 +0000

    Add UnregisterInterestController
    Added and implemented UnregisterInterestController, allowing you to
    sumbit your encoded email, from the unsubscribe link. If the value
    can be decoded it is added to the unsubscribe user queue

---
+++ src/SFA.DAS.Campaign.Web/Controllers/UnregisterInterestController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Campaign.Domain.DataCollection;
+
+namespace SFA.DAS.Campaign.Web.Controllers
+{
+    [Route("un-register")]
+    public class UnregisterInterestController : Controller
+    {
+        private readonly IUserDataCollection _userDataCollection;
+        private readonly IUserDataCryptographyService _userDataCryptographyService;
+
+        public UnregisterInterestController(IUserDataCollection userDataCollection, IUserDataCryptographyService userDataCryptographyService)
+        {
+            _userDataCollection = userDataCollection;
+            _userDataCryptographyService = userDataCryptographyService;
+        }
+
+        [HttpGet]
+        [Route("{email}")]
+        public IActionResult Index(string email)
+        {
+            return View("Index",email);
+        }
+
+        [HttpGet]
+        [Route("thank-you")]
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("submit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Submit(string encodedEmail)
+        {
+            var decodedEmail = _userDataCryptographyService.DecodeUserEmail(encodedEmail);
+
+            if (!string.IsNullOrEmpty(decodedEmail))
+            {
+                await _userDataCollection.RemoveUserData(decodedEmail);
+            }
+
+            return RedirectToAction("ThankYou");
+        }
+    }
+}
