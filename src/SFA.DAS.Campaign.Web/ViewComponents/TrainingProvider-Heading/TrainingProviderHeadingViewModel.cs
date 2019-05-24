10a20b966e269954ba7a233fa60f04e84a193488
Matt Derry <matt.derry@digital.education.gov.uk>
2019-05-24 15:25:23 +0100

    Adds training provider search and results for FAT

---
+++ src/SFA.DAS.Campaign.Web/ViewComponents/TrainingProvider-Heading/TrainingProviderHeadingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Sfa.Das.Sas.ApplicationServices.Models;
+using Sfa.Das.Sas.Shared.Components.ViewComponents.Fat;
+using Sfa.Das.Sas.Shared.Components.ViewComponents.TrainingProvider.Search;
+using Sfa.Das.Sas.Shared.Components.ViewModels;
+
+namespace SFA.DAS.Campaign.Web.ViewComponents
+{
+    public class TrainingProviderHeadingViewModel
+    {
+
+        public ApprenticeshipHeadingViewModel Apprenticeship { get; set; } = new ApprenticeshipHeadingViewModel();
+        public SearchResultsViewModel<TrainingProviderSearchResultsItem, TrainingProviderSearchViewModel> SearchResults { get; set;
+        }
+
+        public int Level { get; set; }
+    }
+}   
