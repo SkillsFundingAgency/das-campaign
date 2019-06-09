631e176a17231fcc27c9b2f42dc0fd4c43b992c0
James King <hello@jamesmichaelking.com>
2019-06-09 20:25:35 +0100

    EOI landing page

---
+++ src/SFA.DAS.Campaign.Web/Controllers/LandingPagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Campaign.Web.Controllers
+{
+
+    public class LandingPagesController : Controller
+    {
+        [Route("eoi")]
+        public IActionResult ExpressionOfInterest()
+        {
+            return View();
+        }
+        [Route("eoi/survey")]
+        public IActionResult ExpressionOfInterestSurvey()
+        {
+            return View();
+        }
+        [Route("eoi/thanks")]
+        public IActionResult ExpressionOfInterestThank()
+        {
+            return View();
+        }
+    }
+}
