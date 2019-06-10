e0cd592c850fdf8c4c90aa2ed1c64097a563ebf8
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-06-10 10:30:23 +0100

    Review changes

--- src/SFA.DAS.Campaign.Web/Controllers/LandingPagesController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/LandingPagesController.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
-
     public class LandingPagesController : Controller
     {
-        [Route("eoi")]
+        [Route("eoi", Name = "ExpressionOfInterest")]
         public IActionResult ExpressionOfInterest()
         {
             return View();
         }
-        [Route("eoi/survey")]
+
+        [Route("eoi/survey", Name = "ExpressionOfInterestSurvey")]
         public IActionResult ExpressionOfInterestSurvey()
         {
             return View();
         }
-        [Route("eoi/thanks")]
+
+        [Route("eoi/thanks", Name = "ExpressionOfInterestThanks")]
         public IActionResult ExpressionOfInterestThank()
         {
             return View();
