8fc5f8b1a4371415dff9ecc7670cdd7a67c846bf
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-06-26 09:00:48 +0100

    Add confirm page

---
+++ src/SFA.DAS.Campaign.Web/Controllers/BasketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Campaign.Web.Models;
+
+namespace SFA.DAS.Campaign.Web.Controllers
+{
+    [Route("employer/basket")]
+    public class BasketController : Controller
+    {
+        [HttpGet]
+        [Route("confirm-save")]
+        public IActionResult Confirm()
+        {
+            return View(new ConfirmSaveViewModel { ApprenticeshipId = "70", Ukprn = 10000028 });
+        }
+    }
+}
---
+++ src/SFA.DAS.Campaign.Web/Models/ConfirmSaveViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Campaign.Web.Models
+{
+    public class ConfirmSaveViewModel
+    {
+        public string ApprenticeshipId { get; set; }
+        public int Ukprn { get; set; }
+    }
+}
