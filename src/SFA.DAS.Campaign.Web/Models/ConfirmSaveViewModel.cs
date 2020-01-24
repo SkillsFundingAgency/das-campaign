ea044f83abc404c8515a61a00c1f77e3d58ece5d
Ben <ben1.stone@education.gov.uk>
2020-01-24 09:59:46 +0000

    removes model definition

--- src/SFA.DAS.Campaign.Web/Models/ConfirmSaveViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.Campaign.Web.Models
-{
-    public class ConfirmSaveViewModel
-    {
-        public string ApprenticeshipId { get; set; }
-        public int Ukprn { get; set; }
-        public int LocationId { get; set; }
-    }
-}
