b9595bc3ba30ffccef1d268649e07fda8337285d
Dave Gouge <davegouge@gmail.com>
2020-04-27 14:15:27 +0100

    Updated basket toast

--- src/SFA.DAS.Campaign.Web/Controllers/BasketController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/BasketController.cs
@@ -8,20 +8,6 @@ namespace SFA.DAS.Campaign.Web.Controllers
     [Route("employer/basket")]
     public class BasketController : Controller
     {
-        [HttpGet(Name = "BasketView")]
-        public IActionResult View()
-        {
-            ApprenticeshipDetailQueryViewModel vm = null;
-
-            if (TempData.ContainsKey("AddRemoveResponse"))
-            {
-                vm = new ApprenticeshipDetailQueryViewModel()
-                    {AddRemoveBasketResponse = JsonConvert.DeserializeObject<AddOrRemoveFavouriteInBasketResponse>((string) this.TempData["AddRemoveResponse"])};
-            }
-
-            return View("Basket/View", vm);
-        }
-        
         [HttpGet]
         [Route("confirm-save")]
         public IActionResult Confirm()
