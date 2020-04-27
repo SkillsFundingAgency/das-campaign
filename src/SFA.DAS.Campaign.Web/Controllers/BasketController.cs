705613bea6e6d575b4c5605a08870424934d35cf
Dave Gouge <davegouge@gmail.com>
2020-04-27 13:50:14 +0100

    Added basket toast to basket page

--- src/SFA.DAS.Campaign.Web/Controllers/BasketController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/BasketController.cs
@@ -1,10 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sfa.Das.Sas.ApplicationServices.Commands;
+using Sfa.Das.Sas.Shared.Components.ViewModels;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
     [Route("employer/basket")]
     public class BasketController : Controller
     {
+        [HttpGet(Name = "BasketView")]
+        public IActionResult View()
+        {
+            ApprenticeshipDetailQueryViewModel vm = null;
+
+            if (TempData.ContainsKey("AddRemoveResponse"))
+            {
+                vm = new ApprenticeshipDetailQueryViewModel()
+                    {AddRemoveBasketResponse = JsonConvert.DeserializeObject<AddOrRemoveFavouriteInBasketResponse>((string) this.TempData["AddRemoveResponse"])};
+            }
+
+            return View("Basket/View", vm);
+        }
+        
         [HttpGet]
         [Route("confirm-save")]
         public IActionResult Confirm()
