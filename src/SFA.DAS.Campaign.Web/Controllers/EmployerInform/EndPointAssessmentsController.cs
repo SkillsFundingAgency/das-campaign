f0d92bedd76b8061159d846a35a8e0b44d6173b6
Ben Stone <ben1.stone@education.gov.uk>
2020-04-29 17:11:43 +0100

    content for training your apprentice page

---
+++ src/SFA.DAS.Campaign.Web/Controllers/EmployerInform/EndPointAssessmentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Campaign.Web.Controllers.EmployerInform
+{
+    public class EndPointAssessmentsController : Controller
+    {
+        [HttpGet("employer/end-point-assessments")]
+        public IActionResult Index()
+        {
+            return View("~/Views/EmployerInform/EndPointAssessments.cshtml");
+        }
+    }
+}
