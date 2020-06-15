a86cc20018a30b00297ccc2e76300c2f0174bb60
Dave Gouge <davegouge@gmail.com>
2020-06-15 11:09:17 +0100

    Postcode validation

--- src/SFA.DAS.Campaign.Web/Controllers/EmployerInspire/WhereIsYourOrganisationBasedController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/EmployerInspire/WhereIsYourOrganisationBasedController.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Campaign.Web.Helpers;
 
@@ -18,12 +20,20 @@ namespace SFA.DAS.Campaign.Web.Controllers.EmployerInspire
         {
             var inspireJourneyChoices = _sessionService.Get<InspireJourneyChoices>(typeof(InspireJourneyChoices).Name) ?? new InspireJourneyChoices();
             
-            return View("~/Views/EmployerInspire/WhereIsYourOrganisationBased.cshtml", inspireJourneyChoices);
+            var vm = new InspirePostcodeViewModel();
+            vm.Postcode = inspireJourneyChoices.Postcode;
+
+            return View("~/Views/EmployerInspire/WhereIsYourOrganisationBased.cshtml", vm);
         }
 
         [HttpPost]
-        public IActionResult Post(InspireJourneyChoices vm)
+        public IActionResult Post(InspirePostcodeViewModel vm)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("~/Views/EmployerInspire/WhereIsYourOrganisationBased.cshtml", vm);
+            }
+            
             var inspireJourneyChoices = _sessionService.Get<InspireJourneyChoices>(typeof(InspireJourneyChoices).Name) ?? new InspireJourneyChoices();
 
             inspireJourneyChoices.Postcode = vm.Postcode;
@@ -33,5 +43,12 @@ namespace SFA.DAS.Campaign.Web.Controllers.EmployerInspire
 
             return RedirectToAction("Index", "YourApprenticeshipAdvice");
         }
+    }
+
+    public class InspirePostcodeViewModel
+    {
+        [Required(ErrorMessage = "Enter your organisation's postcode")]
+        [RegularExpression(@"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})", ErrorMessage = "Enter a real postcode.")]
+        public string Postcode { get; set; }
     }
 }
