8881a12345eaf1baf61911ccb9aca4e04797523a
Dave Gouge <davegouge@gmail.com>
2020-07-22 12:37:58 +0100

    Parents page into CMS

--- src/SFA.DAS.Campaign.Web/Controllers/ParentsController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/ParentsController.cs
@@ -1,19 +1,27 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Campaign.Application.Content;
+using SFA.DAS.Campaign.Application.Content.ContentTypes;
 using SFA.DAS.Campaign.Application.DataCollection;
 using SFA.DAS.Campaign.Web.Models;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
-    [Route("parents")]
     public class ParentsController : Controller
     {
-        [HttpGet]
-        [Route("their-career")]
-        public IActionResult TheirCareer(string email)
-        {
-            return View("TheirCareer");
-        }
+        private readonly IContentService _contentService;
 
+        public ParentsController(IContentService contentService)
+        {
+            _contentService = contentService;
+        }
+        
+        [HttpGet("/parents/their-career")] 
+        public async Task<IActionResult> TheirCareer()
+        {
+            var content = await _contentService.GetContentByHubAndSlug<InfoPage>(HubTypes.Parents, "their-career");
+   
+            return View("TheirCareer", content);
+        }
     }
 }
