0e866f738767d6c0c3b62db55e764bd11ca56a40
Dave Gouge <davegouge@gmail.com>
2020-09-03 19:16:59 +0100

    Some url renames

--- src/SFA.DAS.Campaign.Domain/Content/IContentService.cs
+++ src/SFA.DAS.Campaign.Domain/Content/IContentService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.Campaign.Domain.Content
@@ -5,5 +6,6 @@ namespace SFA.DAS.Campaign.Domain.Content
     public interface IContentService
     {
         Task<Page<T>> GetPage<T>(string slug) where T : IContent;
+        List<ArticleCard> GetArticleCardsFor(string landingPageSlug, HubType hubType);
     }
 }
