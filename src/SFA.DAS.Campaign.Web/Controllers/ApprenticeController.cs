0a52eb645c1052420df2e481916e55635761a404
Paul Howes <paul.howes@cognetics.net>
2020-11-12 16:54:53 +0000

    Removed health check references

--- src/SFA.DAS.Campaign.Web/Controllers/ApprenticeController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/ApprenticeController.cs
@@ -1,17 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.Campaign.Infrastructure.HealthChecks;
 
 namespace SFA.DAS.Campaign.Web.Controllers
 {
     public class ApprenticeController : Controller
     {
-        private readonly IVacancyServiceApiHealthCheck _vacancyServiceApiHealthCheck;
-
-        public ApprenticeController(IVacancyServiceApiHealthCheck healthCheck)
-        {
-            _vacancyServiceApiHealthCheck = healthCheck;
-        }
-        
         [Route("/apprentices/browse-apprenticeships")]
         public IActionResult FindAnApprenticeship()
         {
