1af776b61a773fcad42468ba8fbafa3f57ed410f
Steven Scaife <steven_scaife@hotmail.com>
2021-06-04 17:23:54 +0100

    added the contentful service and some unit tests for retrieving articles

--- src/SFA.DAS.Campaign.Domain/Content/Article.cs
+++ src/SFA.DAS.Campaign.Domain/Content/Article.cs
@@ -1,13 +1,18 @@
 using System.Collections.Generic;
+using Contentful.Core.Models;
 
 namespace SFA.DAS.Campaign.Domain.Content
 {
-    public class Article : IContent
+    public class Article : IContentType
     {
-        public List<ArticleSection> Sections { get; set; }
-        public string LandingPageSlug { get; set; }
-        public string LandingPageTitle { get; set; }
-
+        public SystemProperties Sys { get; set; }
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string PageTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string HubType { get; set; }
+        public LandingPage LandingPage { get; set; }
         public string Summary { get; set; }
+        public List<ArticleSection> Sections { get; set; }
     }
 }
