29f4e284edcfd32d4f66f330e677f90e242ec55a
Steven Scaife <steven_scaife@hotmail.com>
2021-06-24 11:14:15 +0100

    removed unneeded property

--- src/SFA.DAS.Campaign.Infrastructure/Api/Requests/GetArticlesRequest.cs
+++ src/SFA.DAS.Campaign.Infrastructure/Api/Requests/GetArticlesRequest.cs
@@ -8,13 +8,11 @@ namespace SFA.DAS.Campaign.Infrastructure.Api.Requests
     {
         private readonly string _hub;
         private readonly string _slug;
-        private readonly bool _local;
 
-        public GetArticlesRequest(string hub, string slug, bool local)
+        public GetArticlesRequest(string hub, string slug)
         {
             _hub = hub;
             _slug = slug;
-            _local = local;
         }
 
         public string GetUrl => $"article/{_hub}/{_slug}";
