741d51d6b11ab537b07ac6886432c2fcd84ffb35
Steven Scaife <steven_scaife@hotmail.com>
2021-07-16 09:27:46 +0100

    removed redundant commented code

--- src/SFA.DAS.Campaign.MockServer/MockApiServer.cs
+++ src/SFA.DAS.Campaign.MockServer/MockApiServer.cs
@@ -45,14 +45,6 @@ namespace SFA.DAS.Campaign.MockServer
                     .WithStatusCode(200)
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile($"{Directory.GetCurrentDirectory()}/json/landing-page-data-response.json"));
-
-            //server.Given(Request.Create()
-            //        .WithPath(o => string.Compare(o, "/landingpage/apprentices/are-they-right-for-yo", StringComparison.OrdinalIgnoreCase) == 0)
-            //        .UsingGet())
-            //    .RespondWith(Response.Create()
-            //        .WithStatusCode(404)
-            //        .WithHeader("Content-Type", "application/json")
-            //        .WithBodyFromFile($"{Directory.GetCurrentDirectory()}/json/landing-page-not-found-data-response.json"));
         }
 
         private static void AddArticlePageResponses(WireMockServer server)
@@ -64,14 +56,6 @@ namespace SFA.DAS.Campaign.MockServer
                     .WithStatusCode(200)
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile($"{Directory.GetCurrentDirectory()}/json/article-data-response.json"));
-
-            //server.Given(Request.Create()
-            //        .WithPath(o => string.Compare(o, "/article/apprentices/becoming-apprentic", StringComparison.OrdinalIgnoreCase) == 0)
-            //        .UsingGet())
-            //    .RespondWith(Response.Create()
-            //        .WithStatusCode(404)
-            //        .WithHeader("Content-Type", "application/json")
-            //        .WithBodyFromFile($"{Directory.GetCurrentDirectory()}/json/article-not-found-data-response.json"));
         }
 
         private static void AddHubPageResponses(WireMockServer server)
@@ -83,14 +67,6 @@ namespace SFA.DAS.Campaign.MockServer
                     .WithStatusCode(200)
                     .WithHeader("Content-Type", "application/json")
                     .WithBodyFromFile($"{Directory.GetCurrentDirectory()}/json/hub-data-response.json"));
-
-            //server.Given(Request.Create()
-            //        .WithPath(o => string.Compare(o, "/hub/apprentice", StringComparison.OrdinalIgnoreCase) == 0)
-            //        .UsingGet())
-            //    .RespondWith(Response.Create()
-            //        .WithStatusCode(404)
-            //        .WithHeader("Content-Type", "application/json")
-            //        .WithBodyFromFile($"{Directory.GetCurrentDirectory()}/json/hub-not-found-data-response.json"));
         }
 
         private static void AddMenuResponses(WireMockServer server)
