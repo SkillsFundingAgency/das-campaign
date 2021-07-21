0df70f7241d2403b76babc2e335ac9a56d8ab1cb
Steven Scaife <steven_scaife@hotmail.com>
2021-07-21 11:16:23 +0100

    added basic implementation of IRule, needs tests

---
+++ src/SFA.DAS.Campaign.Web/MiddleWare/AddRedirectRulesExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace SFA.DAS.Campaign.Web.MiddleWare
+{
+    public static class AddRedirectRulesExtension
+    {
+        public static void AddRedirectRules(this IApplicationBuilder app)
+        {
+            var options = new RewriteOptions();
+            //Courses
+            options.Add(new PathWithQueryRule("(?i)apprenticeship/searchresults\\b", "/courses?Keyword=$0&Levels=$1", new List<string> { "keywords", "SelectedLevels" }));
+            options.AddRedirect("(?i)apprenticeship/search\\b", "courses", (int)HttpStatusCode.PermanentRedirect);
+            options.AddRedirect("(?i)provider/frameworkresults", "courses", (int)HttpStatusCode.PermanentRedirect);
+            options.AddRedirect("(?i)apprenticeship/searchforframeworkproviders", "courses", (int)HttpStatusCode.PermanentRedirect);
+            options.AddRedirect("(?i)apprenticeship/framework/(.*)", "courses", (int)HttpStatusCode.PermanentRedirect);
+
+            //Course Provider
+            options.Add(new PathWithQueryRule("(?i)provider/detail\\b", "/courses/$0/providers/$1?location=$2", new List<string> { "standardCode", "ukprn", "postcode" }));
+
+            //Course Providers
+            options.Add(new PathWithQueryRule("(?i)provider/standardresults\\b", "/courses/$0/providers?location={1}", new List<string> { "apprenticeshipid", "postcode" }));
+            options.Add(new PathWithQueryRule("(?i)Apprenticeship/SearchForStandardProviders\\b", "/courses/$0/providers", new List<string> { "standardId" }));
+
+            //Home
+            options.AddRedirect("(?i)apprenticeshiporprovider", "/", (int)HttpStatusCode.PermanentRedirect);
+            options.AddRedirect("(?i)apprenticeship/apprenticeshiporprovider", "/", (int)HttpStatusCode.PermanentRedirect);
+            options.AddRedirect("(?i)provider/(.*)", "/", (int)HttpStatusCode.PermanentRedirect);
+
+            //Course
+            options.AddRedirect("(?i)apprenticeship/standard/(.*)", "courses/$1", (int)HttpStatusCode.PermanentRedirect);
+
+            app.UseRewriter(options);
+        }
+    }
+}
---
+++ src/SFA.DAS.Campaign.Web/MiddleWare/PathWithQueryRule.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
+
+namespace SFA.DAS.Campaign.Web.MiddleWare
+{
+    public class PathWithQueryRule : IRule
+    {
+        private readonly string _requestRegexPattern;
+        private readonly string _replacement;
+        private readonly List<string> _originQueryParams;
+
+        public PathWithQueryRule(string requestRegexPattern, string replacement, List<string> originQueryParams)
+        {
+            _requestRegexPattern = requestRegexPattern;
+            _replacement = replacement;
+            _originQueryParams = originQueryParams;
+        }
+
+        public void ApplyRule(RewriteContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            if (request.Path.StartsWithSegments(new PathString(_replacement)))
+            {
+                return;
+            }
+
+            if (!Regex.Match(request.Path.Value, _requestRegexPattern, RegexOptions.IgnoreCase).Success)
+            {
+                return;
+            }
+
+            var location = _replacement;
+
+            for (var i = 0; i < _originQueryParams.Count; i++)
+            {
+                var queryStringValues = request.Query[_originQueryParams[i]];
+
+                if (queryStringValues.Count == 1)
+                {
+                    var queryStringValue = queryStringValues.FirstOrDefault();
+
+                    if (!string.IsNullOrEmpty(queryStringValue))
+                    {
+                        location = location.Replace($"${i}", queryStringValue);
+                    }
+                }
+                else if (queryStringValues.Count > 1)
+                {
+                    var lastAmpIndex = location.LastIndexOf("&", StringComparison.CurrentCultureIgnoreCase);
+                    var valIndex = location.IndexOf($"${i}", StringComparison.CurrentCultureIgnoreCase);
+
+                    var subStringVal = location.Substring(lastAmpIndex, valIndex - lastAmpIndex);
+                    var newQueryListParam = queryStringValues.Aggregate("", (current, stringValue) => current + subStringVal + stringValue);
+                    location = location.Replace($"{subStringVal}${i}", newQueryListParam);
+                }
+            }
+
+            while (location.Contains("$"))
+            {
+                var index = location.IndexOf("$", StringComparison.CurrentCultureIgnoreCase);
+                location = location.Remove(index, 2);
+            }
+
+
+            var response = context.HttpContext.Response;
+            response.StatusCode = (int)HttpStatusCode.PermanentRedirect;
+            context.Result = RuleResult.EndResponse;
+            response.Headers[HeaderNames.Location] = location;
+
+        }
+    }
+}
