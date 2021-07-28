782a885835830070e3ee677cf661b15bde3ae973
Steven Scaife <steven_scaife@hotmail.com>
2021-07-28 11:13:27 +0100

    fixed failing tests

--- src/SFA.DAS.Campaign.UnitTests/Web/Renderers/WhenTabbedPageRenderer.cs
+++ src/SFA.DAS.Campaign.UnitTests/Web/Renderers/WhenTabbedPageRenderer.cs
@@ -3,7 +3,11 @@ using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.NUnit3;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.VisualBasic;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.Campaign.Domain.Content;
 using SFA.DAS.Campaign.Domain.Content.HtmlControl;
@@ -14,21 +18,13 @@ namespace SFA.DAS.Campaign.UnitTests.Web.Renderers
     public class WhenTabbedContentRenderer
     {
         [Test, AutoData]
-        public void Is_Passed_An_Object_Of_IHtmlControl_That_Is_Of_Tabs_Then_Supports_Content_Returns_True(TabbedContentRenderer renderer)
+        public void Is_Passed_An_Object_Of_IHtmlControl_That_Is_Of_Tabs_Then_Supports_Content_Returns_True()
         {
-            var fixture = new Fixture();
-            fixture.Customize(new AutoMoqCustomization
-            {
-                ConfigureMembers = true
-            });
-
-            var controls = fixture.CreateMany<IHtmlControl>();
+            TabbedContentRenderer renderer = new TabbedContentRenderer();
+          
             var tabs = new Tabs();
             var tabbedContent = new List<TabbedContent>();
-            tabbedContent.Add(new TabbedContent
-            {
-                Content = controls
-            });
+            tabbedContent.Add(new TabbedContent());
 
             var actual = renderer.SupportsContent(tabs);
 
@@ -36,36 +32,38 @@ namespace SFA.DAS.Campaign.UnitTests.Web.Renderers
         }
 
         [Test, AutoData]
-        public void Is_Passed_An_Object_Of_IHtmlControl_That_Is_Not_Of_Tabs_Then_Supports_Content_Returns_False(Table table, TabbedContentRenderer renderer)
+        public void Is_Passed_An_Object_Of_IHtmlControl_That_Is_Not_Of_Tabs_Then_Supports_Content_Returns_False(Table table)
         {
+            TabbedContentRenderer renderer = new TabbedContentRenderer();
             var actual = renderer.SupportsContent(table);
-            
+
             actual.Should().BeFalse();
         }
 
         [Test, AutoData]
-        public void Is_Passed_An_Object_Of_TabbedContent_Then_Render_Returns_The_Html(TabbedContentRenderer renderer)
+        public void Is_Passed_An_Object_Of_TabbedContent_Then_Render_Returns_The_Html()
         {
+            var renderer = new TabbedContentRenderer();
+            var context = new Mock<HttpContext>();
+            var tempDictionary = new Mock<ITempDataDictionary>();
+
+            renderer.HttpContext = context.Object;
+            renderer.TempDataDictionary = tempDictionary.Object;
+
             var tabs = new Tabs();
-            var tabbedContent = new List<TabbedContent>();
-            
-            tabbedContent.Add(new TabbedContent
+            var tabbedContent = new List<TabbedContent>
             {
-                TabTitle = "Title",
-                TabName = "TabName",
-                Content = new List<IHtmlControl>()
+                new TabbedContent
                 {
-                    new Heading
+                    TabTitle = "Title",
+                    TabName = "TabName",
+                    Content = new List<IHtmlControl>()
                     {
-                        HeadingSize = 2, 
-                        Content = new List<string>()
-                        {
-                            "a heading"
-                        }
+                        new Heading {HeadingSize = 2, Content = new List<string>() {"a heading"}}
                     }
                 }
-            });
-
+            };
+            
             tabs.TabbedContents = tabbedContent;
 
             var actual = renderer.Render(tabs);
