44c0e726c6a955abf5c07172bd4e53bb16adc936
Sarah <sarah.coetzee@education.gov.uk>
2022-09-29 15:50:56 +0100

    Seperate view into partials

--- src/SFA.DAS.Campaign.Web/Controllers/EmployerController.cs
+++ src/SFA.DAS.Campaign.Web/Controllers/EmployerController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Campaign.Application.Content.Queries;
+using SFA.DAS.Campaign.Application.FundingTool;
 using SFA.DAS.Campaign.Domain.ApprenticeshipCourses;
 using SFA.DAS.Campaign.Infrastructure.Configuration;
 using SFA.DAS.Campaign.Web.Helpers;
@@ -16,13 +17,13 @@ namespace SFA.DAS.Campaign.Web.Controllers
         private readonly IMediator _mediator;
         private readonly IStandardsRepository _repository;
 
-        public EmployerController (IOptions<CampaignConfiguration> configuration, IMediator mediator, IStandardsRepository repository)
+        public EmployerController(IOptions<CampaignConfiguration> configuration, IMediator mediator, IStandardsRepository repository)
         {
             _configuration = configuration.Value;
             _mediator = mediator;
             _repository = repository;
         }
-        
+
         [Route("/employers/find-apprenticeship-training")]
         [Route("/employer/find-apprenticeship-training")]
         public IActionResult FindApprenticeshipTraining()
@@ -37,8 +38,8 @@ namespace SFA.DAS.Campaign.Web.Controllers
             slug2 = "future-proof-your-business";
             var standards = _repository.GetStandards(null);
             var staticContent = _mediator.GetModelForStaticContent();
-            var panel1 = _mediator.Send(new GetPanelQuery() { Slug = slug1, Preview = true});
-            var panel2 = _mediator.Send(new GetPanelQuery() {Slug = slug2, Preview = true});
+            var panel1 = _mediator.Send(new GetPanelQuery() { Slug = slug1, Preview = true });
+            var panel2 = _mediator.Send(new GetPanelQuery() { Slug = slug2, Preview = true });
 
             await Task.WhenAll(staticContent, panel1, panel2);
 
@@ -49,6 +50,17 @@ namespace SFA.DAS.Campaign.Web.Controllers
                 BannerModels = staticContent.Result.BannerModels,
                 Panel1 = panel1.Result.Panel,
                 Panel2 = panel2.Result.Panel
+            });
+        }
+
+        [HttpPost("/employers/calculate-your-apprenticeship-funding")]
+        public IActionResult CalculateApprenticeshipFunding(bool payBillGreaterThanThreeMillion, bool? overFiftyEmployees, Standard trainingCourse, int numberRoles)
+        {
+            var input = new CalculationInputValues() { PayBillGreaterThanThreeMillion = payBillGreaterThanThreeMillion, OverFiftyEmployees = overFiftyEmployees, TrainingCourse = trainingCourse, NumberRoles = numberRoles };
+            var result = input.CalculateFundingAndTraining();
+            return View(new ApprenticeshipFundingViewModel
+            {
+                Calculation = result
             });
         }
     }
--- src/SFA.DAS.Campaign.Web/Models/ApprenticeshipFundingViewModel.cs
+++ src/SFA.DAS.Campaign.Web/Models/ApprenticeshipFundingViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using SFA.DAS.Campaign.Application.FundingTool;
 using SFA.DAS.Campaign.Domain.Content;
 using SFA.DAS.Campaign.Domain.Content.HtmlControl;
+using Standard = SFA.DAS.Campaign.Domain.Content.Standard;
 
 namespace SFA.DAS.Campaign.Web.Models
 {
@@ -12,6 +14,7 @@ namespace SFA.DAS.Campaign.Web.Models
         public Panel Panel1 { get; set; }
         public Panel Panel2 { get; set; }
         public Panel Panel3 { get; set; }
+        public CalculationOutputValues Calculation { get; set; }
 
     }
 }
