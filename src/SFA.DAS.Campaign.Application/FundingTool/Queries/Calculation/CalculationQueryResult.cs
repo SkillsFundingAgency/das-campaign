60b9f526ea77d5e5ebed9217eaa6ba8f68f8b723
Sarah <sarah.coetzee@education.gov.uk>
2022-10-26 11:51:52 +0100

    Remove comments

--- src/SFA.DAS.Campaign.Application/FundingTool/Queries/Calculation/CalculationQueryResult.cs
+++ src/SFA.DAS.Campaign.Application/FundingTool/Queries/Calculation/CalculationQueryResult.cs
@@ -13,30 +13,5 @@ namespace SFA.DAS.Campaign.Application.FundingTool.Queries.Calculation
         public int Duration { get; set; }
         public int Level { get; set; }
         public string Title { get; set; } = null!;
-
-        //public string? Format(int? number)
-        //{
-        //    if (number == null) { return null; }
-
-        //    var newNumber = number.Value.ToString();
-
-        //    switch (newNumber.Length)
-        //    {
-        //        case 4:
-        //            return newNumber.Insert(1, ",");
-        //        case 5:
-        //            return newNumber.Insert(2, ",");
-        //        case 6:
-        //            return newNumber.Insert(3, ",");
-        //        case 7:
-        //            newNumber = newNumber.Insert(1, ",");
-        //            newNumber = newNumber.Insert(5, ",");
-        //            return newNumber;
-        //        default:
-        //            return newNumber;
-
-        //    }
-        //}
-
     }
 }
