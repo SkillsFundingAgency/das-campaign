249f316fc940ecc7d4385486ef8b13a4c32ce4b5
James King <hello@jamesmichaelking.com>
2023-02-03 16:33:56 +0000

    Change to the error tag helper

--- src/SFA.DAS.Campaign.Web/TagHelpers/ValidationTagHelper.cs
+++ src/SFA.DAS.Campaign.Web/TagHelpers/ValidationTagHelper.cs
@@ -1,27 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
 
 namespace SFA.DAS.Campaign.Web.TagHelpers
 {
-    [HtmlTargetElement("div", Attributes = "validation-row-status")]
+    [HtmlTargetElement("div", Attributes = "highlight-error-for")]
     public class ValidationTagHelper : TagHelper
     {
-        [ViewContext]
-        public ViewContext ViewContext { get; set; } = null!;
+        private const string HighlightErrorForAttributeName = "highlight-error-for";
+        private const string ErrorCssClass = "govuk-form-group--error";
 
-        public string PropertyName { get; set; } = null!;
+        [HtmlAttributeName(HighlightErrorForAttributeName)]
+        public ModelExpression Property { get; set; }
+
+        [HtmlAttributeName(ErrorCssClass)]
+        public string CssClass { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            if (PropertyIsInvalid())
-                output.Attributes.Add("class", "govuk-form-group--error");
-        }
+            if (!ViewContext.ModelState.ContainsKey(Property.Name)) return;
 
-        bool PropertyIsInvalid()
-        {
-            return ViewContext?.ModelState[PropertyName]?.ValidationState == ModelValidationState.Invalid;
+            var modelState = ViewContext.ModelState[Property.Name];
+
+            if (modelState.Errors.Count == 0) return;
+            output.AddClass(ErrorCssClass, HtmlEncoder.Default);
         }
     }
 }
