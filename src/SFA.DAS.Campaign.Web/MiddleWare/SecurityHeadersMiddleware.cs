bf5f39509e3ee921a281bad913e589dc19f7ff10
Jordan Singh <jordan.singh@fujitsu.com>
2023-12-08 11:22:19 +0000

    removing comments

--- src/SFA.DAS.Campaign.Web/MiddleWare/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.Campaign.Web/MiddleWare/SecurityHeadersMiddleware.cs
@@ -12,15 +12,7 @@ namespace SFA.DAS.Campaign.Web.MiddleWare
 
         public async Task InvokeAsync(HttpContext context)
         {
-            //context.Response.Headers.AddIfNotPresent("x-frame-options", new StringValues("SAMEORIGIN"));
-
-            /* TEMPORARILY REMOVING THESE AS ONE OF THEM IS INCORRECTLY BLOCKING GOOGLE ANALYTICS*/
-            //context.Response.Headers.AddIfNotPresent("x-content-type-options", new StringValues("nosniff"));
-            //context.Response.Headers.AddIfNotPresent("X-Permitted-Cross-Domain-Policies", new StringValues("none"));
-            //context.Response.Headers.AddIfNotPresent("x-xss-protection", new StringValues("0"));
-            //context.Response.Headers.AddIfNotPresent("Content-Security-Policy", new StringValues($"default-src *; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; img-src *; style-src * 'unsafe-inline'; object-src *;"));
-            var requestUri = context.Request.GetUri();
-            var authority = requestUri.Authority;
+            var authority = context.Request.GetUri().Authority;
             const string dasCdn = " das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
             
             context.Response.Headers.AddIfNotPresent("x-frame-options", new StringValues("DENY"));
@@ -34,7 +26,7 @@ namespace SFA.DAS.Campaign.Web.MiddleWare
                     $"style-src 'self' 'unsafe-inline' {dasCdn} *.typekit.net https://tagmanager.google.com https://fonts.googleapis.com https://*.rcrsv.io ; " +
                     $"img-src {dasCdn} {authority} https://t.co ib.adnxs.com www.facebook.com bat.bing.com px.ads.linkedin.com www.facebook.com https://analytics.twitter.com www.google.com www.google.co.uk www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://*.google-analytics.com ; " +
                     $"font-src {dasCdn} *.typekit.net https://fonts.gstatic.com https://*.rcrsv.io data: ;" +
-                    $"connect-src  https://*.google-analytics.com https://*.zendesk.com https://*.zdassets.com wss://*.zopim.com https://*.rcrsv.io ;"));
+                    $"connect-src wss://localhost:* https://t.co 665-cpw-432.mktoresp.com stats.g.doubleclick.net px.ads.linkedin.com *.snapchat.com https://consent-api-bgzqvpmbyq-nw.a.run.app https://*.google-analytics.com https://*.zendesk.com https://*.zdassets.com wss://*.zopim.com https://*.rcrsv.io ;"));
 
             await next(context);
         }
