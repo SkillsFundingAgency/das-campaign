72fe898beb5a34c25221fffcf9f445a16921e04d
David Rimar <david.rimar@digital.education.gov.uk>
2024-07-22 16:10:31 +0100

    EC-539 remove OverFiftyEmployees

--- src/SFA.DAS.Campaign.Application/FundingTool/Queries/Calculation/CalculationQuery.cs
+++ src/SFA.DAS.Campaign.Application/FundingTool/Queries/Calculation/CalculationQuery.cs
@@ -5,7 +5,6 @@ namespace SFA.DAS.Campaign.Application.FundingTool.Queries.Calculation
     public class CalculationQuery : IRequest<CalculationQueryResult>
     {
         public bool PayBillGreaterThanThreeMillion { get; set; }
-        public bool? OverFiftyEmployees { get; set; }
         public Standard TrainingCourse { get; set; }
         public int NumberRoles { get; set; }
     }
