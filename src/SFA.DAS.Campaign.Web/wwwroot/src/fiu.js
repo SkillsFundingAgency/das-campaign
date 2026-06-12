955f8ad0d7a43fcd0e3b71cb0e6c9f3ec660e540
James King <hello@jamesmichaelking.com>
2026-06-12 10:07:27 +0100

    Frontend tasks for creating frontend assets

--- src/SFA.DAS.Campaign.Web/wwwroot/src/fiu.js
+++ src/SFA.DAS.Campaign.Web/wwwroot/src/fiu.js
@@ -1,7 +1,6 @@
 import {Autocomplete} from './components/autocomplete/_autocomplete.js';
 import {BackLink} from './components/back-link/_back-link.js';
 import {Banner} from './components/banner/_banner.js';
-import {Navigation} from './components/navigation/_navigation.js';
 import {PrintLink} from './components/print-page/_print-page';
 
 // Autocomplete
@@ -23,12 +22,6 @@ const banners = document.querySelectorAll('[data-fiu-banner]');
 banners.forEach(banner => {
   new Banner(banner).init();
 });
-
-// Navigation
-const nav = document.querySelector('[data-fiu-navigation]');
-if (nav) {
-  let navObj = new Navigation(nav);
-}
 
 // Print Page
 const printPageLinkContainer = document.querySelector('[data-fiu-print-page-link]');
